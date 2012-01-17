12df8a8d37d8493e0ffac07aafaf4511cb44e834
kiennguyen1101 <kiennguyen1101@gmail.com>
2012-01-17 12:45:16 +0700

    - error log

---
+++ core/logs/log-2012-01-17.php
@@ -0,0 +1,3 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-01-17 03:40:14 --> Severity: Notice  --> Undefined variable: userInfo D:wampwwwrbsappcontrollershome.php 210
