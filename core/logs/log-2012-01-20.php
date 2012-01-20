4837ae209601698c776c73b3c7a0dc93c80d3e29
vietanhta <vietanhta@gmail.com>
2012-01-20 18:36:56 +0700

    core/logs/log-2012-01-20.php

--- core/logs/log-2012-01-20.php
+++ core/logs/log-2012-01-20.php
@@ -2,3 +2,4 @@
 
 ERROR - 2012-01-20 09:42:31 --> Severity: Notice  --> Undefined property: Home::$common_models D:\wamp\www\rbs\rbs\app\controllers\home.php 269
 ERROR - 2012-01-20 09:43:13 --> Severity: Notice  --> Trying to get property of non-object D:\wamp\www\rbs\rbs\app\controllers\home.php 262
+ERROR - 2012-01-20 11:33:42 --> Severity: Notice  --> Trying to get property of non-object D:\wamp\www\rbs\rbs\app\controllers\home.php 262
