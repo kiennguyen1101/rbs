cca5def263b8aed51a22d2ef09eae01783efb14e
Nguyen Kien <kiennguyen1101@gmail.com>
2012-01-26 13:10:46 +0700

    - remove urgent conditions in search product - fix a bug when displaying search results

--- app/views/search/listProjects.php
+++ app/views/search/listProjects.php
@@ -35,74 +35,73 @@
             );
 
             //Get Customize data fields
-            if ($this->session->userdata('show_cat')) {               
+            if ($this->session->userdata('show_cat')) {
                 array_push($heading, $this->lang->line('Job Type'));
             }
 
-            if ($this->session->userdata('show_status')) {               
+            if ($this->session->userdata('show_status')) {
                 array_push($heading, $this->lang->line('Status'));
             }
 
-            if ($this->session->userdata('show_bids')) {               
+            if ($this->session->userdata('show_bids')) {
                 array_push($heading, $this->lang->line('Bids'));
             }
 
-            if ($this->session->userdata('show_date')) {             
+            if ($this->session->userdata('show_date')) {
                 array_push($heading, $this->lang->line('Start Date'));
             }
-            
+
             //set table headings
             $this->table->set_heading($heading);
-            
-            if (isset($projects) and $projects->num_rows() > 0) {
+
+            if (isset($projects) && $projects->num_rows() > 0) {
                 $i = 0;
                 foreach ($projects->result() as $project) {
+
                     $t_row = array();
-                    $i=$i+1;
-                    array_push($t_row,$i);
-                    
-                    $prj_name = sprintf('<a href="%s">%s</a>', 
-                            site_url('project/view/'.$project->id),
-                            highlight_phrase($project->project_name,$keyword,'<b>','</b>'));
-                   
+                    $i = $i + 1;
+                    array_push($t_row, $i);
+
+                    $prj_name = sprintf('<a href="%s">%s</a>', site_url('project/view/' . $project->id), highlight_phrase($project->project_name, $keyword, '<b>', '</b>'));
+
                     array_push($t_row, $prj_name);
-                    
+
                     if ($this->session->userdata('show_cat'))
-                        array_push ($t_row, getCategoryLinks($project->project_categories));
-                }
-                
-                if ($this->session->userdata('show_status')) {
-                    $status = getProjectStatus($project->project_status);
-                    if ($status != "Open")
-                        $status = "<span style='color:red;'>$status</span>";
-                    else
-                        $status = "<span style='color:green;'>$status</span>";
-                    array_push ($t_row, $status);
-                }
+                        array_push($t_row, getCategoryLinks($project->project_categories));
+
+
+                    if ($this->session->userdata('show_status')) {
+                        $status = getProjectStatus($project->project_status);
+                        if ($status != "Open")
+                            $status = "<span style='color:red;'>$status</span>";
+                        else
+                            $status = "<span style='color:green;'>$status</span>";
+                        array_push($t_row, $status);
+                    }
+
+
+                    if ($this->session->userdata('show_bids'))
+                        array_push($t_row, getNumBid($project->id));
+
+                    if ($this->session->userdata('show_date'))
+                        array_push($t_row, get_date($project->created));
                     
-                
-                if ($this->session->userdata('show_bids'))
-                    array_push ($t_row, getNumBid($project->id));
-                
-                if ($this->session->userdata('show_date'))
-                    array_push ($t_row, get_date($project->created));
-                
-                $this->table->add_row($t_row);
-                    
+                    $this->table->add_row($t_row);
+                }
             }
-            else {                
+            else {
                 $cell = array('data' => $this->lang->line('No Records'), 'class' => 'help', 'colspan' => count($heading));
                 $this->table->add_row($cell);
-            }            
+            }
 
             //print out the table
             echo $this->table->generate();
             ?>
-          
+
         </form>
         <!--PAGING-->
-        <?php if (isset($pagination))
-            echo $pagination; ?>
+<?php if (isset($pagination))
+    echo $pagination; ?>
         <!--END OF PAGING-->
 
     </div>
